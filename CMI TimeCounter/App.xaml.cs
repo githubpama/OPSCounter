902faa0d8a77ba31c9e412297ce8267a3c252824
PatrickMay <may.patrick@hotmail.com>
2024-11-30 12:10:31 +0100

    initial commit

---
+++ CMI TimeCounter/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace CMI_TimeCounter;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
+{
+}
